774ab002134e849afb69049975cf22657cef1648
oliverxekwalla <oliverekwalla@gmail.com>
2022-05-01 19:58:16 +0100

    user model/controller added

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express'); 
 const app = express(); 
 const mongoose = require('mongoose');
-require('dotenv/config')
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+require('dotenv/config'); 
+const bodyParser = require('body-parser'); 
+const cors = require('cors'); 
 
+// Middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+// Import Routes
 const blogRoute = require('./controllers/blog');
 const userRoute = require('./controllers/user');
 
 app.use('/api/blogs', blogRoute)
-// app.use('/user', userRoute)
+app.use('/api/users', userRoute)
 
-app.get("/", (req, res)=>{
-    res.send("hello world")
-})
-
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL,()=>{
     console.log('DB connected')
 })
--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,32 @@
-// const express = require('express'); 
+const express = require('express'); 
+const userRouter = express.Router();
+const bcrypt = require('bcrypt'); 
+
+const User = require('../models/User')
+
+userRouter.get('/', async (req, res)=>{
+    try{
+        const users = await User.find({}); 
+        res.json(users); 
+    } catch (err){
+        res.status(404).json({message : err}); 
+    }
+})
+
+userRouter.post('/', async (req, res)=>{
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(req.body.password, saltRounds);
+
+    const user = new User({
+        username: req.body.username, 
+        passwordHash
+    })
+
+    const savedUser = await user.save();
+    res.json(savedUser);
+
+})
 
 
-// module.exports = userRouter; 
+module.exports = userRouter; 
