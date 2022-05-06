f58fbed73125d7c148ad3917c512897ed6efd59f
oliverxekwalla <oliverekwalla@gmail.com>
2022-05-06 17:19:54 +0100

    navbar UI completed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -26,7 +26,7 @@ function Header() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
                 <div className="flex-shrink-0 flex items-center">
                   {/* <img
                     className="block lg:hidden h-8 w-auto"
@@ -38,7 +38,7 @@ function Header() {
                     src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
                     alt="Workflow"
                   /> */}
-                  <h2 className="text-orange-500">Blogsite</h2>
+                  <h2 className="text-orange-500"><a className="no-underline text-inherit hover:text-orange-700" href="/" >Blogsite</a></h2>
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 left-0">
                   {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
@@ -50,8 +50,9 @@ function Header() {
                   </a> */}
                   <a
                     href="/login"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-3 pt-1 text-medium font-medium"
+                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-orange-500 inline-flex items-center px-3 pt-1 text-medium font-medium no-underline"
                   >
+
                     Login
                   </a>
                   {/* <a
@@ -63,7 +64,7 @@ function Header() {
                 
                      <a
                     href="/register"
-                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 "
+                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 no-underline "
                   >
                        Register
                   </a>
