7b18c03dfc30983a848779b61350c327ba71217b
oliverxekwalla <oliverekwalla@gmail.com>
2022-05-20 14:48:51 +0100

    able to DELETE blog on client

--- front/src/components/IndividualBlog.js
+++ front/src/components/IndividualBlog.js
@@ -1,18 +1,23 @@
 import axios from 'axios'; 
 import React, { useEffect, useState } from 'react'; 
 import { useSelector, useDispatch} from 'react-redux';
-import { useLocation } from "react-router";
+import { useLocation} from "react-router";
+import { useNavigate} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { AiFillEdit } from "react-icons/ai"
+import { AiFillDelete } from "react-icons/ai"
 
 function IndividualBlog() {
 
     const [individualBlog, setIndividualBlog ] = useState(''); 
-     const [editMode, Edit] = useState(false); 
+    const [editMode, Edit] = useState(false); 
     const [username, setUsername] = useState('');
    
     const location = useLocation();
+    const navigate = useNavigate(); 
     const path = location.pathname.split("/")[2];
+
+    const user = useSelector((state)=> state.user); 
     
     useEffect(()=>{
         axios
@@ -20,18 +25,39 @@ function IndividualBlog() {
             .then(res=>setIndividualBlog(res.data)); 
     }, [])
 
+   
+    // const handleDelete = (e) => {
+    //   e.preventDefault(); 
+    //    axios.delete(`http://localhost:3002/api/blogs/${path}`)
+    //    .then(()=>navigate("http://localhost:3000/allblogs")
+    // }
+
+    const deleteRequest = async () => {
+        const res = await axios.delete(`http://localhost:3002/api/blogs/${path}`).catch(err=>console.log(err)); 
+        const data = await res.data;
+    }
+
+    const handleDelete = () => {
+          //  axios.delete(`http://localhost:3002/api/blogs/${path}`,
+          //  { data: { username: user.username }})
+          deleteRequest().then(()=>navigate("/allblogs"))
+          
+    }
+
     console.log(individualBlog)
   return (
   
-      <div className="relative px-4 sm:px-6 lg:px-8 font-mono">
+      <div className="relative px-4 mt-10 sm:px-6 lg:px-8 font-mono">
         <div className='flex-shrink-0 aspect-w-16 aspect-h-9'>
-            <img className="h-64 w-64  md:w-1/2 md:h-96 rounded-lg object-cover m-auto" src={individualBlog.picture} alt="" />
+            <img className="h-auto w-64  md:w-1/2 md:h-96 rounded-lg object-cover m-auto" src={individualBlog.picture} alt="" />
         </div>
         <div className="text-lg max-w-prose mx-auto">
           <h1>
             <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               {individualBlog.title}
-              <FontAwesomeIcon icon="fas fa-edit" />
+              <AiFillEdit />
+              
+              <AiFillDelete onClick={handleDelete}/>
             </span>
              <span className=" md:flex block text-base text-center text-orange-600 font-semibold tracking-wide space-between gap-x-96 uppercase">
               <p> Author: {individualBlog.author}</p>
